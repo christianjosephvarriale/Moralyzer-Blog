c8014ba04d13537faa2b64f770b8dfbb1515fac9
Christian <cjvarria@uwaterloo.ca>
2021-01-19 16:42:06 -0500

    add build to release

---
+++ client/build/precache-manifest.ca4e7dc1de2f476296eae53ea2da3e37.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79d14990da2a792677c3ddcfd1f13b3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee51f5bad2bdce2c77b4",
+    "url": "/static/css/2.8f0d8aca.chunk.css"
+  },
+  {
+    "revision": "002123b5481c780b64bc",
+    "url": "/static/css/main.1d0b43bb.chunk.css"
+  },
+  {
+    "revision": "ee51f5bad2bdce2c77b4",
+    "url": "/static/js/2.86e04d9d.chunk.js"
+  },
+  {
+    "revision": "002123b5481c780b64bc",
+    "url": "/static/js/main.05004749.chunk.js"
+  },
+  {
+    "revision": "baf82305e79f0dc9f8c1",
+    "url": "/static/js/runtime~main.469930a7.js"
+  },
+  {
+    "revision": "2af9e98848a2429f2b9b1b02ba07a754",
+    "url": "/static/media/404.2af9e988.png"
+  },
+  {
+    "revision": "c4bc9fa81b643004a7fbbd8ed4dd27c3",
+    "url": "/static/media/cbc.c4bc9fa8.png"
+  },
+  {
+    "revision": "bebb2687aea5dc6a9d5c577621645a4c",
+    "url": "/static/media/ctvNews.bebb2687.png"
+  },
+  {
+    "revision": "25a10153d29270b7b047c9b8c8266c42",
+    "url": "/static/media/fa-brands-400.25a10153.woff"
+  },
+  {
+    "revision": "877700a37b705486d19bdce3f7586754",
+    "url": "/static/media/fa-brands-400.877700a3.woff2"
+  },
+  {
+    "revision": "9c500ffe5dc5b39629b9b71c7c5dc9d6",
+    "url": "/static/media/fa-brands-400.9c500ffe.eot"
+  },
+  {
+    "revision": "a5b88231dac1cda8a4fc0695dae61d73",
+    "url": "/static/media/fa-brands-400.a5b88231.ttf"
+  },
+  {
+    "revision": "ea5813ec2914a7adf7c97a932267f9ea",
+    "url": "/static/media/fa-brands-400.ea5813ec.svg"
+  },
+  {
+    "revision": "1d244b608b6d96473fa7b46a621bd079",
+    "url": "/static/media/fa-regular-400.1d244b60.ttf"
+  },
+  {
+    "revision": "21e5fcc513f73160350188271e209dff",
+    "url": "/static/media/fa-regular-400.21e5fcc5.eot"
+  },
+  {
+    "revision": "4af5c592b6ae36bab3c4c22826e355a3",
+    "url": "/static/media/fa-regular-400.4af5c592.svg"
+  },
+  {
+    "revision": "559f1b6e24fa052808a4f4e0e8243b83",
+    "url": "/static/media/fa-regular-400.559f1b6e.woff2"
+  },
+  {
+    "revision": "a6a973facdfbfe6aa7e903d778fe4402",
+    "url": "/static/media/fa-regular-400.a6a973fa.woff"
+  },
+  {
+    "revision": "2621e1e8d43fc0ec0fbb7245539e802e",
+    "url": "/static/media/fa-solid-900.2621e1e8.svg"
+  },
+  {
+    "revision": "32b1e75e26fc6396745e030a9d834197",
+    "url": "/static/media/fa-solid-900.32b1e75e.woff"
+  },
+  {
+    "revision": "4533d2cb6e29e96fc26e8044f6d5be00",
+    "url": "/static/media/fa-solid-900.4533d2cb.eot"
+  },
+  {
+    "revision": "64f88ebe25c54a12320f42f0b229961b",
+    "url": "/static/media/fa-solid-900.64f88ebe.woff2"
+  },
+  {
+    "revision": "c691fb6dffedbb292dc21b1cdbd3a27c",
+    "url": "/static/media/fa-solid-900.c691fb6d.ttf"
+  },
+  {
+    "revision": "3ae13bc863b5aebd90257119aa4b22f0",
+    "url": "/static/media/fb.3ae13bc8.svg"
+  },
+  {
+    "revision": "4ee6a08fbbd4ff698d5a3a6e184b2a46",
+    "url": "/static/media/globe.4ee6a08f.png"
+  },
+  {
+    "revision": "385c43541348dee7cadb92d666562cfa",
+    "url": "/static/media/huff.385c4354.png"
+  },
+  {
+    "revision": "7c32430ad47467205a8371793d3e45c1",
+    "url": "/static/media/icon-arrow-left.7c32430a.svg"
+  },
+  {
+    "revision": "d0b9b6b6639634d8dedcf82d06a2557a",
+    "url": "/static/media/icon-arrow-right.d0b9b6b6.svg"
+  },
+  {
+    "revision": "af3b3e5d68cbb2e020ec8cd34cb39f82",
+    "url": "/static/media/icon-arrow-up.af3b3e5d.svg"
+  },
+  {
+    "revision": "ba3dc621a638fd024295bf6640c4ac5c",
+    "url": "/static/media/icon-mail.ba3dc621.svg"
+  },
+  {
+    "revision": "9f870d1c4cf5591d4996ed5ea14f579a",
+    "url": "/static/media/icon-minus.9f870d1c.svg"
+  },
+  {
+    "revision": "7715e82bbda9218d2991d2b02e054246",
+    "url": "/static/media/icon-plus.7715e82b.svg"
+  },
+  {
+    "revision": "19014f1329d44c807b50237069a10a79",
+    "url": "/static/media/icon-search.19014f13.svg"
+  },
+  {
+    "revision": "b1c58ee19d1e2c38b29ae86f3174f2e4",
+    "url": "/static/media/instagram.b1c58ee1.svg"
+  },
+  {
+    "revision": "717a9f4b80b7139021c6c3ff41ef1716",
+    "url": "/static/media/logo.717a9f4b.png"
+  },
+  {
+    "revision": "a60ac307a29a999290e6bec1815eee0f",
+    "url": "/static/media/metro.a60ac307.png"
+  },
+  {
+    "revision": "6537c2eec531ac1660cedaedd9fcf127",
+    "url": "/static/media/montreal.6537c2ee.png"
+  },
+  {
+    "revision": "61350f1aeb14626ed6ff69a02f9a7001",
+    "url": "/static/media/personCoding.61350f1a.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "1bb04703c694acbe067b4b539075538a",
+    "url": "/static/media/virus.1bb04703.png"
+  }
+]);
